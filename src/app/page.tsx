2a6818c9d12adcb363f5500b662247eeac78c21b
Amir <haghighi251@gmail.com>
2024-06-07 20:42:32 +0200

    feat: add flowbite as the ui framework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HomePage from "@/pages/Home/HomePage";
 import Image from "next/image";
 
 export default function Home() {
   return (<div>
     <h1>root page.tsx file</h1>
+    <HomePage />
   </div>);
 }
